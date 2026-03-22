0ac94a6b8434d411c27c6d2c79ffbac87c79336a
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2026-03-22 18:50:00 +0100

    Day 5 CRUD

--- index.js
+++ index.js
@@ -25,6 +25,39 @@ app.get("/test", (req, res) => {
   res.json(message);
 });
 
+app.post("/jobs", (req, res) => {
+  const { title, company } = req.body;
+
+  if (!title || !company) {
+    return res.json({
+      message: "Title and company are required",
+    });
+  }
+
+  const newJob = {
+    id: Date.now(),
+    title,
+    company,
+  };
+
+  jobs.push(newJob);
+
+  res.json({
+    message: "Job added",
+    job: newJob,
+  });
+});
+
+app.delete("/jobs/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  jobs = jobs.filter((job) => job.id !== id);
+
+  res.json({
+    message: "Job deleted",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
