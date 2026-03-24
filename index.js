3dab979d296d2f01412110f4751d318ae789cfa0
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2026-03-24 18:50:15 +0100

    PATCH

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let jobs = [
   { id: 2, title: "Backend developer", company: "Amazon" },
 ];
 
+const allowedStatus = ["applied", "interview", "rejected"];
+
 let message = { message: "Backend works" };
 
 app.get("/", (req, res) => {
@@ -34,7 +36,6 @@ app.get("/test", (req, res) => {
 
 app.post("/jobs", (req, res) => {
   const { title, company, status } = req.body;
-  const allowedStatus = ["applied", "interview", "rejected"];
 
   if (status && !allowedStatus.includes(status)) {
     return res.status(400).json({ error: "Invalid status" });
@@ -69,6 +70,52 @@ app.delete("/jobs/:id", (req, res) => {
   res.json({
     message: "Job deleted",
   });
+});
+
+app.patch("/jobs/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updates = req.body;
+  const job = jobs.find((job) => job.id === id);
+  const allowedFields = ["title", "company", "status"];
+  const incomingFields = Object.keys(req.body);
+  const index = jobs.findIndex((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({
+      error: "Not Found",
+    });
+  }
+
+  if (incomingFields.length === 0) {
+    return res.status(400).json({
+      error: "Empty Request",
+    });
+  }
+
+  const isValidFields = incomingFields.every((field) =>
+    allowedFields.includes(field),
+  );
+
+  if (!isValidFields) {
+    return res.status(400).json({
+      error: "Bad Request",
+    });
+  }
+
+  if (updates.status && !allowedStatus.includes(updates.status)) {
+    return res.status(400).json({
+      error: "invalid status",
+    });
+  }
+
+  const updatedJob = {
+    ...job,
+    ...req.body,
+  };
+
+  jobs[index] = updatedJob;
+
+  return res.json(updatedJob);
 });
 
 app.listen(3000, () => {
